58170a548f4905236870eff47989e4b834eaf027
Nicholas Welch <nicholaswelch1989@gmail.com>
2017-06-16 20:55:58 -0700

    Add all the doc requirements unable to get question 7 working

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 'use strict'
 
+var correctAnswer = 0;
+
 function  yesQuestions(answer){
 
   if (answer.toUpperCase() === 'YES' || answer.toUpperCase() === 'Y'){
-    alert('You are not a Zombie!')
+    alert('You are not a Zombie!');
+    correctAnswer++;
   }else{
-    alert('Your infected with the T-Virus!')
+    alert('Your infected with the T-Virus!');
   }
 };
 function noQuestions(answer){
   if (answer.toUpperCase() === 'No' || answer.toUpperCase() === 'N'){
-    alert('You are not a Zombie!')
+    alert('You are not a Zombie!');
+    correctAnswer++;
   }else{
-    alert('Your infected with the T-Virus!')
+    alert('Your infected with the T-Virus!');
   }
 };
-yesQuestions(prompt('You want play a dangerous game?'))
-noQuestions(prompt('Your in class and you hear a noise outside the door. Do you want check it out?'))
-yesQuestions(prompt('Would you run out of the building if there was a zombie?'))
-noQuestions(prompt('You outside the classroom and you see a classmate being eatten alive?!?! Do you save them?'))
-noQuestions(prompt('All seems lost, Do you take your own live vs being eatten by the unholy undead?'))
+yesQuestions(prompt('You want play a dangerous game?'));
+noQuestions(prompt('Your in class and you hear a noise outside the door. Do you want check it out?'));
+yesQuestions(prompt('Would you run out of the building if there was a zombie?'));
+noQuestions(prompt('You outside the classroom and you see a classmate being eatten alive?!?! Do you save them?'));
+noQuestions(prompt('All seems lost, Do you take your own live vs being eatten by the unholy undead?'));
 
 var question6 = prompt('How many times am I going to re-take this class? Answer 1-10.');
 for (var answer6 = 0; answer6 < 4 ; answer6++) {
@@ -35,22 +39,26 @@ if (parseInt(question6 < 2)){
   console.log('User got the right answer.')
   alert('You are correct....maybe.');
   answer6 = 4;
+  correctAnswer++
 }
 }
-// //issue with 7 unable to get working correctly
-// var question7 = prompt('What are some of my favorite games?');
-// var answer7 = ['Call of Duty','Ark','Dying Light'];
-// for (var answer7 = 0; answer7 < 4; answer7++) {
-// if (question7 === answer7[0,1,2]){  //answer7[0], || answer7[1], || answer7[2]) {
-// alert ('Good Job!');
-// console.log('User right answer.');
-// }else{
-// alert('Nope');
-// console.log('User wrong answer.');
-// question7 = prompt('Try again.');
-// answer7 = 4;
-// }
-// }
+//issue with 7 unable to get working correctly
+var question7 = prompt('What are some of my favorite games?');
+var answer7 = ['Call of Duty','Ark','Dying Light'];
+for (var answer7 = 0; answer7 < 4; answer7++) {
+if (question7 === answer7[0,1,2]){  //answer7[0], || answer7[1], || answer7[2]) not sure about this
+alert ('Good Job!');
+console.log('User right answer.');
+correctAnswer++
+}else{
+alert('Nope');
+console.log('User wrong answer.');
+question7 = prompt('Try again.');
+answer7 = 4;
+}
+}
+
+document.write ('You got ' + correctAnswer + 'out of 7 correct.');
 
 //No longer need code below just showing work.
 // //question 1
