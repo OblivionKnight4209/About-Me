97303185d409f21147e6838e7fc4990d966f6a85
Jayms8462 <Jayms.Portune@gmail.com>
2017-07-07 16:42:31 -0700

    Made a few changes to the code, syntax and other. Notes of changes in read me

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # About-Me
 This is a simple website showcasing information about me!
+James Contributions -
+  Style.css
+    cleaned up the spacing to make it uniform.
+  Index.html
+    Cleaned up a few Grammar Errors
+    Fixes a few spacing issues to make the code uniform
+  app.js
+    Function noQUestions changed if statement to accept the NO prompt when o is lower case. would not accept the answer no which is correct if the user put an upper N and lower o.
+    Spacing Issues fixed to make code uniform
+    added a few missing semicolons at the end of a few statements
+    
--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 'use strict'
-
+debugger;
 var correctAnswer = 0;
 
-function  yesQuestions(answer){
+function  yesQuestions(answer) {
 
-  if (answer.toUpperCase() === 'YES' || answer.toUpperCase() === 'Y'){
+  if (answer.toUpperCase() === 'YES' || answer.toUpperCase() === 'Y') {
     alert('You are not a Zombie!');
     correctAnswer++;
-  }else{
+  } else {
     alert('Your infected with the T-Virus!');
-  }
+  };
 };
-function noQuestions(answer){
-  if (answer.toUpperCase() === 'No' || answer.toUpperCase() === 'N'){
+function noQuestions(answer) {
+  if (answer.toUpperCase() === 'NO' || answer.toUpperCase() === 'N') {
     alert('You are not a Zombie!');
     correctAnswer++;
-  }else{
+  } else {
     alert('Your infected with the T-Virus!');
-  }
+  };
 };
 
     yesQuestions(prompt('You want play a dangerous game?'));
@@ -29,36 +29,36 @@ function noQuestions(answer){
 
 var question6 = prompt('How many times am I going to re-take this class? Answer 1-10.');
   for (var answer6 = 0; answer6 < 4 ; answer6++) {
-    if (parseInt(question6 < 2)){
-      console.log('User is too low.')
+    if (parseInt(question6 < 2)) {
+      console.log('User is too low.');
       alert('You are too Low.');
+      question6 = prompt('Guess Again.');
+    } else if (question6 > 2) {
+      console.log('User is too High');
+      alert('You are too High.');
   question6 = prompt('Guess Again.');
-}   else if (question6 > 2){
-    console.log('User is too High')
-    alert('You are too High.');
-  question6 = prompt('Guess Again.');
-}   else if (question6 == 2){
-    console.log('User got the right answer.')
-    alert('You are correct....maybe.');
-  answer6 = 4;
-  correctAnswer++
-}
-}
+    } else if (question6 == 2) {
+      console.log('User got the right answer.');
+      alert('You are correct....maybe.');
+      answer6 = 4;
+      correctAnswer++;
+    }
+  }
 //issue with 7 unable to get working correctly
 var question7 = prompt('What are some of my favorite games?');
 var answer7 = ["Call of Duty","Ark","Dying Light"];
 var a = answer7.indexOf();
 for (var a = 0; answer7 < 4; answer7++) {
-  if(answer7.indexOf(question7 !== -1)){  //answer7[0], || answer7[1], || answer7[2]) not sure about this
+  if(answer7.indexOf(question7 !== -1)) {  //answer7[0], || answer7[1], || answer7[2]) not sure about this
     alert ('Good Job!');
     console.log('User right answer.');
-    correctAnswer++
-} else{
+    correctAnswer++;
+  } else {
     alert('Nope');
     console.log('User wrong answer.');
     question7 = prompt('Try again.');
     answer7 = 4;
-}
+  }
 }
 
 document.write ('You got ' + correctAnswer + ' out of 7 correct.');
